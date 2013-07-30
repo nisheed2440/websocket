1d63f7d922799dc9786096545e8b8caafb6ce0c7
Nisheed Jagadish <nisheed2016@gmail.com>
2013-07-30 13:55:31 +0530

    Changed response to match CSS classes

--- socketApp.js
+++ socketApp.js
@@ -4,25 +4,25 @@
 
 //Initialize the express server
 var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+, server = require('http').createServer(app)
+, io = require('socket.io').listen(server);
 
 var tiltDirection = function(x,y,z){
-	
-	var dir = '';
-	
+
+	var dir = 'stop';
+
 	if(Math.round(x,4) > 4.0000){
-		dir = "TILT LEFT";
-    }
-    else if(Math.round(x,4) < -4.0000){
-    	dir = "TILT RIGHT";
-    }
-    else if(Math.round(y,4) < -4.0000){
-    	dir = "TILT FORWARD";
-    }
-    else if(Math.round(y,4) > 4.0000){
-    	dir = "TILT BACKWARD";
-    }
+		dir = 'left';
+	}
+	else if(Math.round(x,4) < -4.0000){
+		dir = 'right';
+	}
+	else if(Math.round(y,4) < -4.0000){
+		dir = 'forward';
+	}
+	else if(Math.round(y,4) > 4.0000){
+		dir = 'backward';
+	}
 	return dir;
 };
 
@@ -32,59 +32,59 @@ server.listen(8080);
 
 //Serve the requested page as index by default
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+	res.sendfile(__dirname + '/index.html');
 });
 
 //Serve the requested mouse display client
 app.get('/client.html', function (req, res) {
-  res.sendfile(__dirname + '/client.html');
+	res.sendfile(__dirname + '/client.html');
 });
 
 app.get('/img/pin.png', function (req, res) {
-  res.sendfile(__dirname + '/img/pin.png');
+	res.sendfile(__dirname + '/img/pin.png');
 });
 
 
 //Phone Client page
 app.get('/client-two.html', function (req, res) {
-  res.sendfile(__dirname + '/client-two.html');
+	res.sendfile(__dirname + '/client-two.html');
 });
 
 //Resources JS
 app.get('/js/jquery-1.10.2.min.js', function (req, res) {
-  res.sendfile(__dirname + '/js/jquery-1.10.2.min.js');
+	res.sendfile(__dirname + '/js/jquery-1.10.2.min.js');
 });
 
 //Resources CSS
 app.get('/css/bootstrap.min.css', function (req, res) {
-  res.sendfile(__dirname + '/css/bootstrap.min.css');
+	res.sendfile(__dirname + '/css/bootstrap.min.css');
 });
 app.get('/css/bootstrap-responsive.min.css', function (req, res) {
-  res.sendfile(__dirname + '/css/bootstrap-responsive.min.css');
+	res.sendfile(__dirname + '/css/bootstrap-responsive.min.css');
 });
 
 //When a socket is connected run the following
 io.sockets.on('connection', function (socket) {
 
-  console.log('Client Connected..');	
-  socket.emit('client connected',{message:'Connected successfully'});
+	console.log('Client Connected..');	
+	socket.emit('client connected',{message:'Connected successfully'});
 
-  //When user sends data to the server
-  socket.on('accel data', function (data) {
-     var coordinateData = {};
-     coordinateData.x = data.posX;
-     coordinateData.y = data.posY;
-     coordinateData.z = data.posZ;
-     
-     var tiltDir = tiltDirection(data.posX,data.posY,data.posZ);
-     
-     //Send this event to the client to do whatever
-     io.sockets.emit('update accel data',{coordinateData:tiltDir});
-  });
-  
-  //When socket is disconnected
-  socket.on('disconnect', function (data) {
-	console.log('Client Disconnected..');
-    io.sockets.emit('user disconnected');
-  });
+	//When user sends data to the server
+	socket.on('accel data', function (data) {
+		var coordinateData = {};
+		coordinateData.x = data.posX;
+		coordinateData.y = data.posY;
+		coordinateData.z = data.posZ;
+
+		var tiltDir = tiltDirection(data.posX,data.posY,data.posZ);
+
+		//Send this event to the client to do whatever
+		io.sockets.emit('update accel data',{coordinateData:tiltDir});
+	});
+
+	//When socket is disconnected
+	socket.on('disconnect', function (data) {
+		console.log('Client Disconnected..');
+		io.sockets.emit('user disconnected');
+	});
 });
